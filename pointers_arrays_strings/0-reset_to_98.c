9d0a22143e9f456a3f2f01b8874bd89a63ff7d59
root <zayymngomezulu@gmail.com>
2025-06-10 11:55:16 +0000

    Initialized pointers_arrays_strings

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates the value
+ * it points to to 98.
+ * @n: A pointer to an integer.
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98; /* Dereference the pointer and assign the value 98 */
+}

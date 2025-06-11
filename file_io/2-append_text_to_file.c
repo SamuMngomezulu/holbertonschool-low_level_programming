536868a61b941a9ad7d2d7e23fcb15e9dec357e5
root <zayymngomezulu@gmail.com>
2025-06-11 13:39:43 +0000

    Update: 2-append_text_to_file.c missing function

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -28,3 +28,19 @@ int append_text_to_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
